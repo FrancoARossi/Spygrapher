5c2b40cbefba341c59a889b6a81a56d24ba1c4ff
FrancoARossi <rossifrancoa@gmail.com>
2019-01-29 20:33:48 -0300

    Added dynamic source management

--- spygrapher.py
+++ spygrapher.py
@@ -1,12 +1,15 @@
-import pyautogui, pyscreeze, time, os, zipfile
+import pyautogui, pyscreeze, time, os, zipfile, sys
 
 ''' Thing to do in order of importance'''
 
-#TODO dinamic image to locate
 #TODO if a folder doesn't exist, create it
 #TODO implement email attachments
 
 current_path = os.path.dirname(os.path.realpath(__file__))
+data_dir = current_path + '/data/'
+sources_dir = current_path + '/sources/'
+source_files = [file for file in os.listdir(sources_dir) if file.endswith('.png')]
+i = 0
 
 def getCurrentDateTime():
 	current_date_time = time.localtime()
@@ -14,7 +17,6 @@ def getCurrentDateTime():
 	return formatted_time
 
 def deleteImages():
-	data_dir = current_path + '/data/'
 	for file in os.listdir(data_dir):
 		if file.endswith('.png'):
 			os.remove(os.path.join(data_dir, file))
@@ -22,7 +24,6 @@ def deleteImages():
 def createZip():
 	date_time = getCurrentDateTime()
 	zip_file = zipfile.ZipFile(current_path + '/compressed_data/images_' + date_time + '.zip', mode='w', compression=zipfile.ZIP_DEFLATED)
-	data_dir = current_path + '/data/'
 	for file in os.listdir(data_dir):
 		if file.endswith('.png'):
 			zip_file.write(os.path.join(data_dir, file))
@@ -37,16 +38,16 @@ def takeScreenshots(amount, interval):
 if __name__ == '__main__':
 	while True:
 		try:
-			pyautogui.locateOnScreen(current_path +'/sources/google_logo_1.png', confidence = 0.9) # Locate the image on screen with a confidence of 90%
-			takeScreenshots(10, 0.5) # When it's located take 10 screenshots with a interval of 4 seconds
-			createZip()
-			deleteImages()
-		except pyscreeze.ImageNotFoundException:
-			try:
-				pyautogui.locateOnScreen(current_path + '/sources/google_logo_2.png', confidence=0.9)
-				takeScreenshots(10, 4)
+			i = i % len(source_files)
+			if i < len(source_files):
+				pyautogui.locateOnScreen(sources_dir + source_files[i], confidence = 0.9) # Locate the image on screen with a confidence of 90%
+				takeScreenshots(10, 0.5) # When it's located take 10 screenshots with a interval of 4 seconds
 				createZip()
 				deleteImages()
-			except pyscreeze.ImageNotFoundException:
-				pass
-		time.sleep(1) # Reduces the cpu usage by about 10% while the image isn't located. Total usage between 3.5% and 5%
+		except pyscreeze.ImageNotFoundException:
+			i += 1
+			time.sleep(1) # Reduces the cpu usage by about 10% while the image isn't located. Total usage between 3.5% and 5%
+			continue
+		except ZeroDivisionError:
+			print('Error: The source folder does not containg any .png file to look for.')
+			sys.exit()
